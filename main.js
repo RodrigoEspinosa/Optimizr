b4c46c9189c70816a66dfa0057f140b66feed277
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2014-03-22 16:55:23 -0300

    UpdateExpression to Expression function

--- main.js
+++ main.js
@@ -9,6 +9,7 @@
 
 var fs = require('fs');
 var esprima = require('esprima');
+var escodegen = require('escodegen');
 
 /* Open the file */
 // Get the filename
@@ -30,6 +31,48 @@ var readNode = function (treeNode, callback) {
   }
 };
 
+var createBlockStatement = function (statements) {
+  return {
+    'type': 'BlockStatement',
+    'body': statements
+  }
+};
+
+var createWhileStatement = function (testExpression, updateExpression, bodyStatement) {
+  var body = createBlockStatement([updateExpression, bodyStatement])
+  return {
+    'type': 'WhileStatement',
+    'test': testExpression,
+    'body': body
+  }
+};
+
+var updateToExpression = function (update) {
+  return {
+    'type': 'ExpressionStatement',
+    'expression': {
+      'type': 'AssignmentExpression',
+      'operator': '=',
+      'left': update.argument,
+      'right': {
+        'type': 'BinaryExpression',
+        'operator': (update.operator == '++') ? '+' : '-',
+        'left': update.argument,
+        'right': {
+          'type': 'Literal',
+          'value': 1,
+          'raw': '1'
+        }
+      }
+    }
+  }
+};
+
+var forToWhile = function (forStatement) {
+  var update = (forStatement.update.type === 'UpdateExpression') ? updateToExpression(forStatement.update) : forStatement.update;
+  return createWhileStatement(forStatement.test, update, forStatement.body);
+};
+
 // Check if file exists
 if (fs.existsSync(filename)) {
   // Read the file
@@ -43,10 +86,14 @@ if (fs.existsSync(filename)) {
     readNode(ast['body'], function (node) {
       if (typeof node === 'object') {
         if (node.type === 'ForStatement') {
-          console.log('init');
-          console.log('test');
-          console.log('update');
-          console.log('body');
+          // console.log('init');
+          // console.log('test');
+          // console.log('update');
+          // console.log('body');
+          var newWhile = forToWhile(node);
+          var output = escodegen.generate(newWhile);
+
+          console.log(output);
         }
       }
     });
